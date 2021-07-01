ae1f588c8af4e4160f64f7a63777a0749ff3a71e
Samed Zırhlıoğlu <zirhlioglusamed@gmail.com>
2021-07-01 04:18:10 +0300

    v1.0

---
+++ Eforsuz/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿using System.Windows.Forms;
+
+namespace EFORSUZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(937, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(955, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ÇEVİR VE KOPYALA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Silver;
+            this.textBox2.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 63);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(1068, 246);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(1092, 321);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1108, 360);
+            this.MinimumSize = new System.Drawing.Size(1108, 360);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EFORSUZ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Eforsuz/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFORSUZ
+{
+    public partial class Form1 : Form
+    {
+        string[] lane1 = { "░░", "███░", "██░░", "███░", "███░", "███░░", "███░", "███░", "███░", "░█░░", "█░█░", "███░", "░█░░", "░░█░", "█░░█░", "█░░░", "█████░", "█░░░█░", "███░", "█░█░", "███░", "░███░░░", "███░░", "███░", "███░", "███░", "█░░█░", "█░░█░", "█░░░█░", "█░░░█░", "█░░░█░", "█░░░█░", "███░" };
+        string[] lane2 = { "░░", "█░█░", "█░█░", "█░░░", "█░░░", "█░░█░", "█░░░", "█░░░", "█░░░", "███░", "█░█░", "░█░░", "███░", "░░█░", "█░█░░", "█░░░", "█░█░█░", "██░░█░", "█░█░", "███░", "█░█░", "█░░░█░░", "█░░█░", "█░░░", "█░░░", "░█░░", "█░░█░", "░░░░░", "█░░░█░", "█░░░█░", "░█░█░░", "░█░█░░", "░░█░" };
+        string[] lane3 = { "░░", "███░", "██░░", "█░░░", "█░░░", "█░░█░", "███░", "██░░", "█░█░", "█░░░", "███░", "░█░░", "░█░░", "░░█░", "██░░░", "█░░░", "█░█░█░", "█░█░█░", "█░█░", "█░█░", "███░", "█░█░█░░", "███░░", "███░", "███░", "░█░░", "█░░█░", "█░░█░", "░█░█░░", "█░█░█░", "░░█░░░", "░░█░░░", "░█░░" };
+        string[] lane4 = { "░░", "█░█░", "█░█░", "█░░░", "███░", "█░░█░", "█░░░", "█░░░", "█░█░", "█░█░", "█░█░", "░█░░", "░█░░", "█░█░", "█░█░░", "█░░░", "█░░░█░", "█░░██░", "█░█░", "█░█░", "█░░░", "█░░██░░", "█░░█░", "░░█░", "░░█░", "░█░░", "█░░█░", "█░░█░", "░███░░", "█░█░█░", "░█░█░░", "░░█░░░", "█░░░" };
+        string[] lane5 = { "░░", "█░█░", "██░░", "███░", "░█░░", "███░░", "███░", "█░░░", "███░", "███░", "█░█░", "███░", "███░", "███░", "█░░█░", "███░", "█░░░█░", "█░░░█░", "███░", "███░", "█░░░", "░███░█░", "█░░█░", "███░", "███░", "░█░░", "████░", "████░", "░░█░░░", "█████░", "█░░░█░", "░░█░░░", "███░" };
+
+        char[] harfler = { ' ', 'a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'ğ', 'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ö', 'p', 'q', 'r', 's', 'ş', 't', 'u', 'ü', 'v', 'w', 'x', 'y', 'z' };
+        int length = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Process();
+            }
+        }
+
+        public void Process()
+        {
+            textBox2.Text = GetLane(textBox1.Text.ToLower());
+
+            char[] karakter = textBox2.Text.ToCharArray();
+            length = karakter.Length;
+
+            Clipboard.Clear();
+            Clipboard.SetText(textBox2.Text);
+
+            if (karakter.Length > 200)
+                label1.Text = "Mesajınız, 200 karakter sınırını aşmıştır. League of Legends ve Valorant gibi oyunlarda, mesajınızın tamamını göremeyebilirsiniz.";
+
+            else if (karakter.Length >= 130)
+                label1.Text = "Mesajınız, League of Legends özel mesaj kısmında düzgün gözükmeyebilir.";
+
+            else
+                label1.Text = null;
+        }
+
+        public string GetLane(string label)
+        {
+            string lastLabel = null;
+            char[] dizi = label.ToCharArray();
+
+            for(int lane = 1; lane <= 5; lane++)
+            {
+                for(int i = 0; i < dizi.Length; i++)
+                {
+                    lastLabel += GetCharacter(dizi[i], lane);
+                }
+                lastLabel += "\r\n";
+            }
+
+            return lastLabel;
+        }
+
+        public string GetCharacter(char harf, int lane)
+        {
+            int harfIndis = FindHarf(harf);
+
+            if (lane == 1) return lane1[harfIndis];
+            else if (lane == 2) return lane2[harfIndis];
+            else if (lane == 3) return lane3[harfIndis];
+            else if (lane == 4) return lane4[harfIndis];
+            else if (lane == 5) return lane5[harfIndis];
+            else return lane1[0];
+        }
+
+        public int FindHarf(char harf)
+        {
+            for(int i = 0; i < harfler.Length; i++)
+            {
+                if (harfler[i] == harf) return i;
+            }
+            return 0;
+        }
+    }
+}
